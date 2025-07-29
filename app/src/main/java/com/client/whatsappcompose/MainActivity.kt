cf67e1d8e798c84286807ad006c3f460ce2ed7b1
ElieOko <elieoko100@gmail.com>
2025-07-29 20:20:49 +0100

    fix:append navigation

--- app/src/main/java/com/client/whatsappcompose/MainActivity.kt
+++ app/src/main/java/com/client/whatsappcompose/MainActivity.kt
@@ -5,18 +5,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.client.whatsappcompose.pages.ParentMain
 import com.client.whatsappcompose.pages.discussions.DiscussionPage
+import com.client.whatsappcompose.routage.NavigationCompose
 import com.client.whatsappcompose.ui.theme.WhatsAppComposeTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navController : NavHostController
     @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            navController = rememberNavController()
             WhatsAppComposeTheme {
-                 ParentMain()
+                NavigationCompose(navController)
             }
         }
     }
